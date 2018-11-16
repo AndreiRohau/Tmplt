fdb5276af0d7ee85ac624c6084b4c76129b54c36
Andrei Rohau <andrei_rohau@epam.com>
2018-11-16 20:59:14 +0300

    initial

--- src/main/java/com/epam/tmpl/Main.java
+++ src/main/java/com/epam/tmpl/Main.java
@@ -1,14 +1,49 @@
 package com.epam.tmpl;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 public class Main {
 
+	private Properties properties;
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
 		System.out.println("tmpl prj");
+
+		Main inst = new Main();
+		String prop = inst.get("tmpl");
+		System.out.println(prop);
 
 	}
 
-	public int i(int i){
+	public int i(int i) {
 		return i;
+	}
+
+	public Main() {
+		try {
+			loadProperties();
+		} catch (IOException e) {
+			System.out.println("Error while Main.class initialization...");
+			e.printStackTrace();
+		}
+	}
+	
+	public String get(String name) {
+		return properties.getProperty(name);
+	}
+
+	private void loadProperties() throws IOException {
+		try (InputStream is = Main.class.getClassLoader().getResourceAsStream("main.properties")) {
+			properties = new Properties();
+			properties.load(is);
+			is.close();
+		} catch (IOException e) {
+			System.out.println("Error while getting properties...");
+			System.out.println(e);
+			e.printStackTrace();
+		}
 	}
 }
--- src/test/java/com/epam/tmpl/MainTest.java
+++ src/test/java/com/epam/tmpl/MainTest.java
@@ -3,16 +3,56 @@ package com.epam.tmpl;
 import com.epam.tmpl.Main;
 import static org.junit.Assert.*;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 import org.junit.Test;
 
 public class MainTest {
 
+	private Properties properties;
+	
 	@Test
 	public void test() {
+		System.out.println("TEST! tmpl prj");
 		
 		Main m = new Main();
 		assertEquals(1, m.i(1));
 	}
-
+	
+	@Test
+	public void testProps() {
+		
+		MainTest m = new MainTest();
+		String prop = m.get("tmpl");
+		
+		System.out.println(prop);
+		
+	}
+	
+	public MainTest() {
+		try {
+			loadProperties();
+		} catch (IOException e) {
+			System.out.println("Error while Main.class initialization...");
+			e.printStackTrace();
+		}
+	}
+	
+	public String get(String name) {
+		return properties.getProperty(name);
+	}
+	
+	private void loadProperties() throws IOException {
+		try (InputStream is = MainTest.class.getClassLoader().getResourceAsStream("test.properties")) {
+			properties = new Properties();
+			properties.load(is);
+			is.close();
+		} catch (IOException e) {
+			System.out.println("Error while getting properties...");
+			System.out.println(e);
+			e.printStackTrace();
+		}
+	}
 }
