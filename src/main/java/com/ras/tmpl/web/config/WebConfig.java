003c507ceebbf9404b2465356915735a44f4e850
Andrei Rohau <andrei_rohau@epam.com>
2018-11-19 11:44:10 +0300

    initia

--- src/main/java/com/ras/tmpl/web/config/WebConfig.java
+++ src/main/java/com/ras/tmpl/web/config/WebConfig.java
@@ -12,7 +12,7 @@ import org.springframework.web.servlet.view.JstlView;
 
 @Configuration
 @EnableWebMvc
-@ComponentScan(basePackages = "com.epam.tmpl")
+@ComponentScan(basePackages = "com.ras.tmpl")
 public class WebConfig extends WebMvcConfigurerAdapter {
 
 	@Bean
